2f97390420c9cc559f3941755dacc4df250c2999
Benjamin Lu <164923145+techkodeninja@users.noreply.github.com>
2025-05-16 08:01:46 +0000

    initial commit

---
+++ app/functions-template.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Template tags.
+ *
+ * This file holds template tags for the theme. Template tags are PHP functions
+ * meant for use within theme templates.
+ *
+ * @package   Momentum
+ * @author    Benjamin Lu <benlumia007@gmail.com>
+ * @copyright 2025 Benjamin Lu
+ * @license   https://www.gnu.org/licenses/gpl-2.0.html
+ * @link      https://luthemes.com/portfolio/momentums
+ */
+
+namespace Momentum;
+
+/**
+ * Returns the metadata separator.
+ *
+ * @since  0.0.1
+ * @access public
+ * @param  string  $sep  String to separate metadata.
+ * @return string
+ */
+function sep( $sep = '' ) {
+
+	return apply_filters(
+		'generosity/sep',
+		sprintf(
+			' <span class="sep">%s</span> ',
+			$sep ?: esc_html_x( '&middot;', 'meta separator', 'generosity' )
+		)
+	);
+}
