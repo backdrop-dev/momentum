04ec9cf587be77aba3b0c003ffa519cb2a9c7624
Benjamin Lu <164923145+techkodeninja@users.noreply.github.com>
2025-05-16 08:13:17 +0000

    initial commit

---
+++ config/_settings-menus.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Momentum Menu Settings Configuration.
+ *
+ * This configuration file defines the menu locations for the Momentum theme.
+ * Each menu location includes a name and is registered during theme setup.
+ *
+ * @package   Momentum
+ * @author    Benjamin Lu <benlumia007@gmail.com>
+ * @copyright 2025 Benjamin Lu
+ * @license   https://www.gnu.org/licenses/gpl-2.0.html
+ * @link      https://luthemes.com/portfolio/momentum
+ */
+
+return [
+    'primary' => [
+        'name' => __( 'Primary Navigation', 'momentum' ),
+    ],
+    'social' => [
+        'name' => __( 'Social Navigation', 'momentum' ),
+    ]
+];
---
+++ config/_settings-sidebars.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Sidebar Settings Configuration.
+ *
+ * This file defines the sidebar locations for the Momentum theme.
+ * These settings are used during theme initialization to register
+ * the available sidebars with their respective names and descriptions.
+ *
+ * @package   Momentum
+ * @author    Benjamin Lu <benlumia007@gmail.com>
+ * @copyright 2025 Benjamin Lu
+ * @license   https://www.gnu.org/licenses/gpl-2.0.html
+ * @link      https://luthemes.com/portfolio/momentum
+ */
+
+return [
+    'primary' => [
+        'name'        => 'Primary Sidebar',
+        'description' => 'The main sidebar for widgets alongside primary content.'
+    ],
+    'footer' => [
+        'name'        => 'Footer Sidebar',
+        'description' => 'A widget area displayed in the footer section of the site.'
+    ],
+];
