d5b8f955df2c67885abb812b7e8e5d48893c4719
Benjamin Lu <164923145+techkodeninja@users.noreply.github.com>
2025-05-18 02:38:26 +0000

    initial commit

---
+++ config/global-layouts.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Global Layouts Config.
+ *
+ * Configuration for the theme's global layouts.
+ *
+ * @package   Momentum
+ * @author    Benjamin Lu <benlumia007@gmail.com>
+ * @copyright 2025 Benjamin Lu
+ * @license   https://www.gnu.org/licenses/gpl-2.0.html
+ * @link      https://luthemes.com/portfolio/momentum
+ */
+
+ return [
+    'left-sidebar' => [
+        'label' => __( 'Left Sidebar', 'momentum' ),
+        'url'   => '%s/public/images/layouts/2cl.png',
+    ],
+    'right-sidebar' => [
+        'label' => __( 'Right Sidebar', 'momentum' ),
+        'url'   => '%s/public/images/layouts/2cr.png',
+    ],
+    'no-sidebar' => [
+        'label' => __( 'No Sidebar', 'momentum' ),
+        'url'   => '%s/public/images/layouts/1col.png',
+    ]
+];
