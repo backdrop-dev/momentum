e2a0bdd37ce8b81c8655c9d6e9ba72ae5c2892e6
Benjamin Lu <164923145+techkodeninja@users.noreply.github.com>
2025-05-18 18:07:46 +0000

    add custom 404 ErrorPage

---
+++ public/views/content/404.php
@@ -0,0 +1,17 @@
+<section id="content" class="site-content">
+<div id="global-layout" <?php Backdrop\Attr\display( Momentum\Tools\Mod::get( 'theme_content_layout' ) ); ?>>
+		<main id="main" class="content-area">
+			<?php $error->setup(); ?>
+			<article id="post-0" class="page">
+				<header class="entry-header">
+					<h1 class="entry-title"><?php $error->displayTitle(); ?></h1>
+				</header>
+				<div class="entry-content">
+					<?php $error->displayContent(); ?>
+				</div>
+			</article>
+			<?php $error->reset(); ?>
+		</main>
+		<?php Backdrop\View\display( 'sidebar', 'primary', [ 'location' => 'primary' ] ); ?>
+	</div>
+</section>
