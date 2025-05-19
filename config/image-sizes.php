440a9b31049d0f8cfadfc04fd3adc680ceeada7d
Benjamin Lu <164923145+techkodeninja@users.noreply.github.com>
2025-05-19 14:47:42 +0000

    initial commit

---
+++ config/image-sizes.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Image Sizes Config.
+ *
+ * Defines the image sizes that the theme sets.
+ *
+ * @package   Momentum
+ * @author    Benjamin Lu <benlumia007@gmail.com>
+ * @copyright 2025 Benjamin Lu
+ * @license   https://www.gnu.org/licenses/gpl-2.0.html
+ * @link      https://luthemes.com/portfolio/momentum
+ */
+
+ return [
+
+	'momentum-landscape-small' => [
+		'label'  => __( 'Landscape: Small', 'momentum' ),
+		'width'  => 640,
+		'height' => 360
+	],
+	'momentum-landscape-medium' => [
+		'label'  => __( 'Landscape: Medium', 'momentum' ),
+		'width'  => 800,
+		'height' => 450
+	],
+	'momentum-landscape-large' => [
+		'label'  => __( 'Landscape: Large', 'momentum' ),
+		'width'  => 1132,
+		'height' => 637
+	],
+];
